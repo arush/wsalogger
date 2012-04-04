078dba37e2525419832d6bcdd327e54d73000055
wsakaren <karen@webshopapps.com>
2012-04-04 14:41:46 +0200

    Removed getVersion logic as not required for logger

--- app/code/community/Webshopapps/Wsalogger/Helper/Data.php
+++ app/code/community/Webshopapps/Wsalogger/Helper/Data.php
@@ -26,42 +26,6 @@
 class Webshopapps_Wsalogger_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
-	/**
-	 * This method will return just the 2nd part of the version number, thus allowing greater than/less than calculations to be performed
-	 * Please convert to this call if possible
-	 * Enter description here ...
-	 */
-	public static function getNewVersion() {
-		$version = Mage::getVersion();	
-		$eeVersion = 0;
-		if (version_compare($version, '1.6', '>=')) {
-			if (version_compare($version, '1.11') >= 0) $eeVersion = 11;
-			else if (version_compare($version, '1.10') >= 0) $eeVersion = 10;
-			else if (version_compare($version, '1.6') >= 0 && !self::isEnterpriseEdition()) $eeVersion = 11;  //CE 1.6, EE unknown at present
-			else if (version_compare($version, '1.9') >= 0) $eeVersion = 9;
-			else if (version_compare($version, '1.8') >= 0) $eeVersion = 8;
-			else if (version_compare($version, '1.7') >= 0) $eeVersion = 7;
-			else if (version_compare($version, '1.6') >= 0) $eeVersion = 6; //EE 1.6
-			else $eeVersion = 8;  // default to this if unsure, changed to assume is 1.4.1+
-		}
-		else {
-			if (version_compare($version, '1.5') >= 0) $eeVersion = 10;
-			else if (version_compare($version, '1.4.2') >= 0) $eeVersion = 9;
-			else if (version_compare($version, '1.4.1') >= 0) $eeVersion = 8;
-			else if (version_compare($version, '1.4.0') >= 0) $eeVersion = 7;
-			else if (version_compare($version, '1.3.2.4') >= 0) $eeVersion = 6;
-			else $eeVersion = 8;  // default to this if unsure, changed to assume is 1.4.1+
-		}
-		return $eeVersion;
-	}
-	
-	
-	public static function isEnterpriseEdition() {
-			if (!Mage::getConfig()->getNode('modules/Enterprise_Cms')) {
-	            return false;
-	        }
-	        return true;
-	}
 	
 public function isDebug($moduleName) {
 		
